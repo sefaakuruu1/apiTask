754b693121d262bc8ab2a3ed20bd0193e6cee06d
sefaakuruu1 <sefaakuruu1@gmail.com>
2023-02-04 21:06:03 +0300

    I created the title part of max 20 characters

--- api.js
+++ api.js
@@ -23,8 +23,17 @@ function displayResult(response) {
     right.classList.add("right");
     //right in cocugu title olusturduk
     let h2 = document.createElement("h2"); //olusturmak istedigin elamanı veriyoruz
-    h2.classList.add("title"); //classın adı buraya verilir
-    h2.innerText = data.title;
+    h2.classList.add("title");
+    let title1 = data.title.length;
+    console.log(title1);
+    console.log(data.title.slice(0, 20));
+    if (title1 > 19) {
+      let sefa = data.title.slice(0, 19);
+      h2.innerText = `${sefa} ...`;
+    } else {
+      h2.innerText = data.title;
+    }
+    //classın adı buraya verilir
     right.appendChild(h2); //bagımsız olmasın diyeparentları ve cocukları yazdık
     body.appendChild(first);
     let p = document.createElement("p");
@@ -36,7 +45,8 @@ function displayResult(response) {
       let paragraph = data.body.split(" ").slice(0, 19).join(" ");
       p.innerText = `${paragraph} ...`;
     } else {
-      p.innerText = description;
+      let paragrap = data.body.split(" ").slice(0, 19).join(" ");
+      p.innerText = paragrap;
     }
     //slice nerde başlayıp nerde biteceğini ister
     right.appendChild(p);
@@ -44,8 +54,12 @@ function displayResult(response) {
     left.classList.add("left");
     first.appendChild(left);
     first.appendChild(right);
+    let leftTwo=document.createElement("div");
+    leftTwo.classList.add("leftTwo");
     let checkbox = document.createElement("input");
     checkbox.type = "checkbox";
+    left.appendChild(leftTwo);
     left.appendChild(checkbox);
+    
   });
 }
